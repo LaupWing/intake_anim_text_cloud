e3d1289f875e1407ba5672cd9630e30869880c12
LaupWing <loc_strict@hotmail.com>
2021-06-18 15:03:54 +0200

    queston

--- partials/Questions.js
+++ partials/Questions.js
@@ -8,7 +8,45 @@ export class Questions{
    set categorie(value){
       const questions = this.questions.filter(question=>question.type === value)
    }
+   populateQuestionContainers(obj){
+      const container = document.body.querySelector('.content')
+      const template = document.body.querySelector('#question-template')
+      const question = template.content.querySelector('.question')
+      const answer = template.content.querySelector('.answer')
+      const question_container = document
+         .createElement('div')
+      question_container.classList
+         .add('question-container')
+      question_container.id = obj.type
+      obj.questions.forEach(q=>{
+         question.querySelector('h3').textContent = q.question
+         q.options.forEach(a =>{
+            console.log(a)
+            answer.textContent = a
+            const answer_container = question.querySelector('.answer-container')
+            answer_container.insertAdjacentElement('beforeend', answer)
+            console.log(question)
+         })
+      })
+      // console.log({
+      //    container,
+      //    questionContainer
+      // })
+   }
+   typesAndQuestion(array){
+      return array
+         .map(x=>x.type)
+         .filter((value, index, self)=> self.indexOf(value) === index)
+         .map(x=>{
+            const questions = array.filter(y=>y.type === x)
+            return {
+               type:x,
+               questions
+            }
+         })
+   }
    init(){
-      console.log(this.header)
+      this.typesAndQuestion(this.questions)
+         .map(type => this.populateQuestionContainers(type))
    }
 }
